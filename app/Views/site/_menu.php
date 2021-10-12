9f4986eb2a0f7ae3d55a9ad56ea6ef96d14ec4f8
borzakap <borzakap@gmail.com>
2021-10-12 17:27:44 +0300

    update by 12.10.2021 set menu items for filter

--- app/Views/site/_menu.php
+++ app/Views/site/_menu.php
@@ -6,7 +6,13 @@
                     <li><a href="<?= route_to('App\Controllers\Pages::index') ?>#home"><?= lang('Site.Menue.Main.Home') ?></a></li>
                     <li><a href="<?= route_to('App\Controllers\Pages::index') ?>#about"><?= lang('Site.Menue.Main.About') ?></a></li>
                     <li><a href="<?= route_to('App\Controllers\Pages::index') ?>#prefs"><?= lang('Site.Menue.Main.Prefs') ?></a></li>
-                    <li><a href="<?= route_to('App\Controllers\Layouts::genplan', 'default') ?>"><?= lang('Site.Menue.Main.LayoutsGenplan') ?></a></li>
+                    <li class="menu-item-has-children">
+                        <a href="javascript:void(0);"><?= lang('Site.Menue.Main.LayoutsMenuSet') ?></a>
+                        <ul class="mb-0 list-unstyled">
+                            <li><a href="<?= route_to('App\Controllers\Layouts::genplan', 'default') ?>" title=""><?= lang('Site.Menue.Main.LayoutsGenplan') ?></a></li>
+                            <li><a href="<?= route_to('App\Controllers\Layouts::filter', 'default') ?>" title=""><?= lang('Site.Menue.Main.LayoutsFilter') ?></a></li>
+                        </ul>
+                    </li>
                     <li><a href="<?= route_to('App\Controllers\Commerce::genplan', 'default') ?>"><?= lang('Site.Menue.Main.CommerceGenplan') ?></a></li>
                 </ul>
             </div>
