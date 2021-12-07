2ab075cfe803f3a4ac31d1bc49c1735f9f8e126b
borzakap <borzakap@gmail.com>
2021-12-07 16:44:52 +0200

    update 07.12.2021 work with transfer

--- app/Controllers/Api/AmoTransfer.php
+++ app/Controllers/Api/AmoTransfer.php
@@ -96,7 +96,7 @@ class AmoTransfer extends BaseController{
 
         // sending to rmanager
         $client = \Config\Services::curlrequest();
-        $response = $client->request('POST', 'https://r-manager.com.ua/api/transfer', ['some' => 'thing']);
+        $response = $client->request('POST', 'https://r-manager.com.ua/api/transfer', ['body' => $return]);
         print_r($response->getBody());
     }
 
