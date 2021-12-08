5b7f5648b91a85313c0da43dff364b2257233d3b
borzakap <borzakap@gmail.com>
2021-12-08 17:30:47 +0200

    update 08.12.2021 work with transfer

--- app/Controllers/Api/AmoTransfer.php
+++ app/Controllers/Api/AmoTransfer.php
@@ -11,6 +11,7 @@ use AmoCRM\Exceptions\AmoCRMApiException;
 use AmoCRM\Models\CustomFieldsValues\ValueCollections\CheckboxCustomFieldValueCollection;
 use AmoCRM\Models\CustomFieldsValues\CheckboxCustomFieldValuesModel;
 use AmoCRM\Models\CustomFieldsValues\ValueModels\CheckboxCustomFieldValueModel;
+use AmoCRM\Collections\CustomFieldsValuesCollection;
 
 /**
  * Description of AmoTransfer
