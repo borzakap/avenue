00a2896fb3a4db428c5cbad8e0f411862e575d23
borzakap <borzakap@gmail.com>
2021-12-16 17:09:55 +0200

    update 16.12.2021 change fix checkbox filter

--- app/Controllers/Api/AmoTransfer.php
+++ app/Controllers/Api/AmoTransfer.php
@@ -115,7 +115,7 @@ class AmoTransfer extends BaseController{
     // finde unprossesed
     private function getUnprossesLeadsId(){
         $filter = new LeadsFilter();
-        $filter->setCreatedAt(time());
+        $filter->setCreatedBy(0);
         $filter->setCustomFieldsValues([591677 => 0]);
         $filter->setOrder('createdAt', \AmoCRM\Filters\Interfaces\HasOrderInterface::SORT_DESC);
         print_r($filter);
